d179860389eff6c06ee0e69e860daad7983497da
zhangyongbin <zhangyongbinkb@163.com>
2020-09-23 11:31:46 +0800

    ApacheCXF 实现WebService（Jax-Rs） 客户端

---
+++ jaxrs-client/src/test/java/com/zyb/ClientTest.java
@@ -0,0 +1,39 @@
+package com.zyb;
+
+import com.zyb.entity.User;
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.junit.Test;
+
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @Description:
+ * @Author: zhangyongbin
+ * @Date: 2020/9/23 10:56
+ */
+public class ClientTest {
+
+    @Test
+    public void testSave(){
+        User user = new User();
+        user.setId(100);
+        user.setUsername("Jerry");
+        user.setCity("gz");
+        // 通过WebClient对象远程调用服务端
+        WebClient
+                .create("http://localhost:8001/jaxrs-server/userService/user")
+                .type(MediaType.APPLICATION_JSON)  // 指定请求的数据格式为json
+                .post(user);
+    }
+
+    @Test
+    public void testGet(){
+        // 查询一个
+        User user =
+                WebClient
+                        .create("http://localhost:8001/jaxrs-server/userService/user/1")
+                        .accept(MediaType.APPLICATION_JSON) // 指定返回的数据格式为json
+                        .get(User.class);
+        System.out.println(user);
+    }
+}
